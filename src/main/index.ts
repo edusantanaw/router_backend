055d22f4d00a8877c7ce54ac5947e3f951715a95
edusantanaw <eduardosantana20122002@gmail.com>
2024-09-18 12:36:06 -0300

    chore: dependêcias inicias e configuração inicial do express

---
+++ src/main/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = Number(process.env.PORT ?? 8080);
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const cb = () => console.log(`Server running at ${PORT}`);
+app.listen(PORT, cb);
