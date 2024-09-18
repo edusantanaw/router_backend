04d6f1d6ff0be05bdc7f78ef881a247257a31f90
edusantanaw <eduardosantana20122002@gmail.com>
2024-09-18 13:43:49 -0300

    feat: customer entity

---
+++ src/domain/entities/customer.ts
@@ -0,0 +1,52 @@
+import { randomUUID } from "node:crypto";
+import { PersonType } from "../enums/personType.enum";
+
+type IData = {
+  id?: string;
+  name: string;
+  personType: PersonType;
+  cpfCnpj: string;
+  dateOfBirth: Date;
+  address: any;
+  createdAt?: Date;
+  active: boolean;
+};
+
+export class Customer {
+  private id: string;
+  private name: string;
+  private personType: PersonType;
+  private cpfCnpj: string;
+  private dateOfBirth: Date;
+  private address: any;
+  private createdAt?: Date;
+  private active: boolean = true;
+
+  constructor(data: IData) {
+    this.id = data.id ?? randomUUID();
+    this.name = data.name;
+    this.address = data.address;
+    this.dateOfBirth = data.dateOfBirth;
+    this.cpfCnpj = data.cpfCnpj;
+    this.personType = data.personType;
+    this.createdAt = data.createdAt;
+    if (data.active !== undefined) this.active = data.active;
+  }
+
+  public get getCustoemr() {
+    return {
+      id: this.id,
+      name: this.name,
+      personType: this.personType,
+      cpfCnpj: this.cpfCnpj,
+      dateOfBirth: this.dateOfBirth,
+      address: this.address,
+      createdAt: this.createdAt,
+      active: this.active,
+    };
+  }
+
+  public disablePerson() {
+    this.active = false;
+  }
+}
---
+++ src/domain/enums/personType.enum.ts
@@ -0,0 +1,5 @@
+
+export enum PersonType {
+    "NORMAL",
+    "LEGAL"
+}
